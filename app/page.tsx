eb0093ac34803a4cc2d3c48fa2c27b26d7b1e174
MarekMurumae <marek.murumae@gmail.com>
2024-09-08 19:41:22 +0300

    fix build error, change messagge pos to bottom of screen

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,6 @@ export default function Chat() {
                 <div className="scroller-content space-y-4" id="scrollerContent">
                     {messages.map(m => (
                         <ChatMessage
-                            className="item"
                             key={m.id}
                             role={m.role}
                             content={m.content}
